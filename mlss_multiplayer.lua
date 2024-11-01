e2360f64fe34dd825776e8507f09632b3820e02c
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2024-11-01 17:03:34 +0100

    Add Type Annotations

--- mlss_multiplayer.lua
+++ mlss_multiplayer.lua
@@ -1,10 +1,8 @@
 local Settings
 local compareTables, copyTable, updateGameData, get_other_player, get_byte_data, get_game_mode, get_front_player, get_battle_player
-local get_input, define_inputs, get_gui_text, redraw_gui_text, main
+local get_input, define_inputs, get_gui_text, redraw_gui_text, main, GameData, exit
 
-math.randomseed(os.time())
-local ProcessID = math.random(10000, 99999)
-
+---@enum GameMode
 local GameMode = {
     MENU = "MODE_MENU",
     BATTLE = "MODE_BATTLE",
@@ -12,6 +10,7 @@ local GameMode = {
     FIELD = "MODE_FIELD"
 }
 
+---@type table<GameMode, string>
 local GameModeStrings = {
     [GameMode.MENU] = "M",
     [GameMode.BATTLE] = "B",
@@ -19,26 +18,21 @@ local GameModeStrings = {
     [GameMode.FIELD] = "F"
 }
 
+---@enum Player
 local Player = {
     MARIO = "MARIO",
     LUIGI = "LUIGI"
 }
 
+---@type table<Player, string>
 local PlayerStrings = {
     [Player.MARIO] = "M",
     [Player.LUIGI] = "L"
 }
 
-local GameData = {
-    byte_data = {},
-    mode = nil,
-    front_player = nil,
-    battle_player = nil,
-    inputs = {},
-    gui_text = {},
-    screen_height = nil
-}
-
+---@param tbl1 table
+---@param tbl2 table
+---@return boolean
 function compareTables(tbl1, tbl2)
     if (tbl1 == tbl2) then
         return true
@@ -60,6 +54,8 @@ function compareTables(tbl1, tbl2)
     return len1 == len2
 end
 
+---@param tbl table
+---@return table
 function copyTable(tbl)
     local tbl_copy = {}
     for key, value in pairs(tbl) do
@@ -68,6 +64,7 @@ function copyTable(tbl)
     return tbl_copy
 end
 
+---@return nil
 function updateGameData()
     GameData.byte_data = get_byte_data()
     GameData.mode = get_game_mode()
@@ -78,6 +75,8 @@ function updateGameData()
     GameData.screen_height = client.screenheight()
 end
 
+---@param player Player
+---@return Player
 function get_other_player(player)
     if (player == Player.MARIO) then
         return Player.LUIGI
@@ -86,7 +85,15 @@ function get_other_player(player)
     end
 end
 
+---@class (exact) ByteData
+---@field is_pause_screen_open boolean
+---@field is_dialog_open boolean
+---@field is_movement_disabled boolean
+---@field front_player_index integer
+
+---@return ByteData
 function get_byte_data()
+    ---@type ByteData
     return {
         is_pause_screen_open = bit.check(memory.readbyte(0x0D5E, "IWRAM"), 0),
         is_dialog_open = bit.check(memory.readbyte(0x03D1, "IWRAM"), 4),       -- active in battle
@@ -99,6 +106,7 @@ function get_byte_data()
     }
 end
 
+---@return GameMode
 function get_game_mode()
     local flags = GameData.byte_data
 
@@ -117,6 +125,7 @@ function get_game_mode()
     end
 end
 
+---@return Player
 function get_front_player()
     local flags = GameData.byte_data
 
@@ -127,6 +136,7 @@ function get_front_player()
     end
 end
 
+---@return Player
 function get_battle_player()
     if ("ACTUAL BATTLE" == "--- TODO ---") then
         if ("MARIO'S TURN" == "--- TODO ---") then
@@ -139,24 +149,60 @@ function get_battle_player()
     end
 end
 
+---@class (exact) GameData
+---@field byte_data ByteData
+---@field mode GameMode
+---@field front_player Player
+---@field battle_player Player
+---@field inputs table<string, boolean>
+---@field gui_text GuiText
+---@field screen_height integer
+
+---@type GameData
+local GameData = {
+    byte_data = get_byte_data(),
+    mode = get_game_mode(),
+    front_player = get_front_player(),
+    battle_player = get_battle_player(),
+    inputs = {},
+    gui_text = {},
+    screen_height = 0
+}
+
+---@class (exact) GbaInput
+---@field Left boolean
+---@field Right boolean
+---@field Up boolean
+---@field Down boolean
+---@field A boolean
+---@field B boolean
+---@field L boolean
+---@field R boolean
+---@field Start boolean
+---@field Select boolean
+---@field Power boolean
+
+---@return GbaInput
 function get_input()
     local game_mode = GameData.mode
     local joy_inputs = GameData.inputs
     local input_map = Settings.input_map
 
+    ---@type GbaInput
     local gba_inputs = {
-        ["Left"] = false,
-        ["Right"] = false,
-        ["Up"] = false,
-        ["Down"] = false,
-        ["A"] = false,
-        ["B"] = false,
-        ["L"] = false,
-        ["R"] = false,
-        ["Start"] = false,
-        ["Select"] = false,
-        ["Power"] = false
+        Left = false,
+        Right = false,
+        Up = false,
+        Down = false,
+        A = false,
+        B = false,
+        L = false,
+        R = false,
+        Start = false,
+        Select = false,
+        Power = false
     }
+
     local front_player = GameData.front_player
     local front_map = input_map[front_player]
     local back_player = get_other_player(front_player)
@@ -172,36 +218,65 @@ function get_input()
         active_map = battle_map
     end
 
-    gba_inputs["Left"] = joy_inputs[active_map.left] or false
-    gba_inputs["Right"] = joy_inputs[active_map.right] or false
-    gba_inputs["Up"] = joy_inputs[active_map.up] or false
-    gba_inputs["Down"] = joy_inputs[active_map.down] or false
-    gba_inputs["Select"] = joy_inputs[active_map.menu] or false
+    gba_inputs.Left = joy_inputs[active_map.left] or false
+    gba_inputs.Right = joy_inputs[active_map.right] or false
+    gba_inputs.Up = joy_inputs[active_map.up] or false
+    gba_inputs.Down = joy_inputs[active_map.down] or false
+    gba_inputs.Select = joy_inputs[active_map.menu] or false
 
     if (game_mode == GameMode.MENU or game_mode == GameMode.BATTLE or game_mode == GameMode.LEVEL_UP) then
-        gba_inputs["L"] = joy_inputs[active_map.menu_L] or false
-        gba_inputs["R"] = joy_inputs[active_map.menu_R] or false
-        gba_inputs["Start"] = joy_inputs[active_map.menu_start] or false
+        gba_inputs.L = joy_inputs[active_map.menu_L] or false
+        gba_inputs.R = joy_inputs[active_map.menu_R] or false
+        gba_inputs.Start = joy_inputs[active_map.menu_start] or false
     end
 
     if (game_mode == GameMode.MENU or game_mode == GameMode.LEVEL_UP) then
-        gba_inputs["A"] = joy_inputs[active_map.menu_confirm] or false
-        gba_inputs["B"] = joy_inputs[active_map.menu_cancel] or false
+        gba_inputs.A = joy_inputs[active_map.menu_confirm] or false
+        gba_inputs.B = joy_inputs[active_map.menu_cancel] or false
     elseif (game_mode == GameMode.BATTLE) then
-        gba_inputs["A"] = joy_inputs[input_map[Player.MARIO].action_perform] or false
-        gba_inputs["B"] = joy_inputs[input_map[Player.LUIGI].action_perform] or false
+        gba_inputs.A = joy_inputs[input_map[Player.MARIO].action_perform] or false
+        gba_inputs.B = joy_inputs[input_map[Player.LUIGI].action_perform] or false
     elseif (game_mode == GameMode.FIELD) then
-        gba_inputs["A"] = joy_inputs[front_map.action_perform] or false
-        gba_inputs["B"] = joy_inputs[back_map.action_perform] or false
-        gba_inputs["L"] = joy_inputs[back_map.action_cycle] or false
-        gba_inputs["R"] = joy_inputs[front_map.action_cycle] or false
-        gba_inputs["Start"] = (joy_inputs[front_map.lead_give] and Settings.allow_lead_give) or
+        gba_inputs.A = joy_inputs[front_map.action_perform] or false
+        gba_inputs.B = joy_inputs[back_map.action_perform] or false
+        gba_inputs.L = joy_inputs[back_map.action_cycle] or false
+        gba_inputs.R = joy_inputs[front_map.action_cycle] or false
+        gba_inputs.Start = (joy_inputs[front_map.lead_give] and Settings.allow_lead_give) or
             (joy_inputs[back_map.lead_take] and Settings.allow_lead_take) or false
     end
 
     return gba_inputs
 end
 
+---@class (exact) InputLayout
+---@field left string
+---@field right string
+---@field up string
+---@field down string
+---@field menu string
+---@field menu_confirm string
+---@field menu_cancel string
+---@field menu_start string
+---@field menu_L string
+---@field menu_R string
+---@field action_perform string
+---@field action_cycle string
+---@field lead_take string
+---@field lead_give string
+
+---@class (exact) Settings
+---@field input_map table<Player,InputLayout>
+---@field allow_lead_take boolean
+---@field allow_lead_give boolean
+---@field log_inputs boolean
+---@field show_gui_rect boolean
+---@field show_mode boolean
+---@field show_front_player boolean
+---@field show_battle_player boolean
+
+---@param controller_name string
+---@param input_table InputLayout
+---@return InputLayout
 function define_inputs(controller_name, input_table)
     local assigned_input_table = {}
     for k, v in pairs(input_table) do
@@ -210,24 +285,32 @@ function define_inputs(controller_name, input_table)
     return assigned_input_table
 end
 
+---@class (exact) GuiText
+---@field mode string?
+---@field front_player string?
+---@field battle_player string?
+
+---@return GuiText
 function get_gui_text()
+    ---@type GuiText
     local gui_text = {}
 
     if (Settings.show_mode) then
-        gui_text["mode"] = GameModeStrings[GameData.mode]
+        gui_text.mode = GameModeStrings[GameData.mode]
     end
 
     if (Settings.show_front_player) then
-        gui_text["front_player"] = PlayerStrings[GameData.front_player]
+        gui_text.front_player = PlayerStrings[GameData.front_player]
     end
 
     if (Settings.show_battle_player) then
-        gui_text["battle_player"] = PlayerStrings[GameData.battle_player]
+        gui_text.battle_player = PlayerStrings[GameData.battle_player]
     end
 
     return gui_text
 end
 
+---@return nil
 function redraw_gui_text()
     gui.use_surface("client")
     gui.clearGraphics()
@@ -235,29 +318,42 @@ function redraw_gui_text()
     local left_offset = 20
 
     local gui_text = GameData.gui_text
-    local mode_text = gui_text["mode"]
-    local front_player_text = gui_text["front_player"]
-    local battle_player_text = gui_text["battle_player"]
 
     if (Settings.show_gui_rect) then
         gui.drawBox(left_offset, top_offset + 3, left_offset + 73, top_offset + 28, "#00000000", "#AA000000")
     end
 
-    if (mode_text ~= nil) then
-        gui.drawString(left_offset, top_offset + 2, mode_text, "#DD667777", "#00000000", 24, nil, "bold")
+    if (gui_text.mode ~= nil) then
+        gui.drawString(left_offset, top_offset + 2, gui_text.mode, "#DD667777", "#00000000", 24, nil, "bold")
     end
 
-    if (front_player_text ~= nil) then
-        gui.drawString(left_offset + 30, top_offset + 2, front_player_text, "#DD667777", "#00000000", 24, nil, "bold")
+    if (gui_text.front_player ~= nil) then
+        gui.drawString(left_offset + 30, top_offset + 2, gui_text.front_player, "#DD667777", "#00000000", 24, nil, "bold")
     end
 
-    if (battle_player_text ~= nil) then
-        gui.drawString(left_offset + 50, top_offset + 2, battle_player_text, "#DD667777", "#00000000", 24, nil, "bold")
+    if (gui_text.battle_player ~= nil) then
+        gui.drawString(left_offset + 50, top_offset + 2, gui_text.battle_player, "#DD667777", "#00000000", 24, nil, "bold")
     end
 end
 
+---@param processID integer
+---@return nil
+function exit(processID)
+    gui.use_surface("client")
+    gui.clearGraphics()
+
+    print("\nMLSS Multiplayer stopped (ID: " .. processID .. ") \n")
+end
+
+---@return nil
 function main()
-    print("\n\n\nMLSS Multiplayer started (ID: " .. ProcessID .. ") \n")
+    math.randomseed(os.time())
+    local processID = math.random(10000, 99999)
+    print("\n\n\nMLSS Multiplayer started (ID: " .. processID .. ") \n")
+
+    event.onexit(function()
+        exit(processID)
+    end)
 
     while true do
         local old_GameData = copyTable(GameData)
@@ -280,13 +376,6 @@ function main()
     end
 end
 
-event.onexit(function()
-    gui.use_surface("client")
-    gui.clearGraphics()
-
-    print("\nMLSS Multiplayer stopped (ID: " .. ProcessID .. ") \n")
-end)
-
 
 
 
@@ -299,7 +388,8 @@ end)
 
 
 
--- The Recommended Input Layout for X-Input compatible Controllers.
+--- The Recommended Input Layout for X-Input compatible Controllers.
+---@type InputLayout
 local RecommendedXInputs = {
     left = "DpadLeft",
     right = "DpadRight",
@@ -317,8 +407,9 @@ local RecommendedXInputs = {
     lead_give = "Y"
 }
 
--- An alternative Input Layout for X-Input compatible Controllers,
--- which is closer to the original controls of the game.
+--- An alternative Input Layout for X-Input compatible Controllers,
+--- which is closer to the original controls of the game.
+---@type InputLayout
 local ClassicXInputs = {
     left = "DpadLeft",
     right = "DpadRight",
@@ -336,7 +427,8 @@ local ClassicXInputs = {
     lead_give = "Start"
 }
 
--- The Recommended Input Layout for the Mayflash N64 Controller Adapter for PC.
+--- The Recommended Input Layout for the Mayflash N64 Controller Adapter for PC.
+---@type InputLayout
 local RecommendedN64Inputs = {
     left = "POV1L",          -- Dpad Left
     right = "POV1R",         -- Dpad Right
@@ -354,8 +446,9 @@ local RecommendedN64Inputs = {
     lead_give = "RotationZ+" -- C Left
 }
 
--- An alternative Input Layout for the Mayflash N64 Controller Adapter for PC,
--- which is closer to the original controls of the game.
+--- An alternative Input Layout for the Mayflash N64 Controller Adapter for PC,
+--- which is closer to the original controls of the game.
+---@type InputLayout
 local ClassicN64Inputs = {
     left = "POV1L",        -- Dpad Left
     right = "POV1R",       -- Dpad Right
@@ -373,7 +466,8 @@ local ClassicN64Inputs = {
     lead_give = "B9"       -- Start
 }
 
--- An Example Layout listing the Controls.
+--- An Example Layout listing the Controls.
+---@type InputLayout
 local ExampleInputs = {
     -- Directional Inputs (Dpad) (only for the Front Player or the current Player in Battle).
     left = "None",
@@ -412,8 +506,9 @@ local ExampleInputs = {
     lead_give = "None"
 }
 
--- Configure the Settings for the Script.
--- Most Settings can be either `true` or `false`, if you want to change them, replace the existing value (eg. replace `false` with `true` or vice versa).
+--- Configure the Settings for the Script.
+--- Most Settings can be either `true` or `false`, if you want to change them, replace the existing value (eg. replace `false` with `true` or vice versa).
+---@type Settings
 Settings = {
     -- Configure Controls for either Player.
     --
