8a93bc84cfe3ecdad92d2e3159a4468919793b25
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2024-11-03 08:11:01 +0100

    Add Screenshots and Savestates to ram export

--- src/scripts/ram_export.lua
+++ src/scripts/ram_export.lua
@@ -5,6 +5,13 @@
 --  a input key for each bucket
 -- TODO: analysis functions
 --  find bytes / bits (that are identifying across all buckets) eg. each bucket has a single unique value
+--  ability to combine buckets for tests (eg. battle_mario, battle_luigi, battle_enemy vs levelup_mario, levelup_luigi)
+--  maybe trough tags (battle, mario, luigi, overworld, movement, ...)
+--  make sure that memory is taken, before buttons are pressed (eg. while you can still make inputs) (battle_mario_attack_before_press, battle_mario_attack_after_press)
+--  ability to detect near misses
+--  store screenshots (and maybe even savestate) with the memory to allow for auditing after the fact
+--  savestate.save(path)
+--  client.screenshot(path)
 
 -- maybe use many addresses for each detection and log unexpected deviations (they should all give the same result)
 -- prepare tests for each flag
@@ -18,6 +25,8 @@
 --  textbox v overworld
 --  cutscene v overworld
 --  pause + textbox + cutscene v battle
+
+
 
 local frames_i = 0
 local iterations_i = 0
@@ -36,7 +45,11 @@ while (iterations_i < iterations) do
         iterations_i = iterations_i + 1
 
         local bytes = memory.read_bytes_as_array(0x0000, 0x7FFF, "IWRAM")
-        local file = io.open("ramdump_" .. id .. "_" .. iterations_i .. ".bin", "wb")
+        local filename = "../../logs/snapshots/snapshot_" .. id .. "_" .. iterations_i
+
+        client.screenshot(filename .. ".png")
+        savestate.save(filename .. ".State", true)
+        local file = io.open(filename .. ".bin", "wb")
 
         if (file == nil) then
             error("File could not be created")
