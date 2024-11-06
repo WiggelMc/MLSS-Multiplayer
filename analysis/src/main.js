ebe98d0e1e58b2a98ecc75cc6294ad60244d02f3
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2024-11-06 08:50:49 +0100

    Add File Loading to Analysis

--- analysis/src/main.js
+++ analysis/src/main.js
@@ -2,13 +2,74 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 
 /**
+* @typedef {object} SearchSet
+* @property {string} SearchFile.name
+* @property {string[]} SearchFile.buckets
+*/
+
+/**
 * @typedef {object} SearchFile
-* @property {string[][]} SearchFile.set
+* @property {SearchSet[]} SearchFile.sets
 * @property {boolean} SearchFile.verbose
 */
 
+/**
+* @typedef {Map<string, number[][]>} ByteCounts
+*/
+
+/**
+ * 
+ * @param {SearchSet} set
+ * @returns {number[][]}
+ */
+function loadSearchSet(set) {
+    const byteCounts = []
+
+    for (const dirName of set.buckets) {
+        const dirPath = path.join("/data/buckets/", dirName)
+
+        for (const fileName of fs.readdirSync(dirPath)) {
+            const filePath = path.resolve(dirPath, fileName)
+
+            const buffer = fs.readFileSync(filePath, {flag: "r"})
+            for (const [i, byte] of buffer.entries()) {
+                
+                const byteCount = byteCounts[i] ?? []
+                byteCount[byte] = (byteCount[byte] ?? 0) + 1
+    
+                byteCounts[i] = byteCount
+            }
+        }
+    }
+
+    return byteCounts
+}
+
+/**
+* @typedef {object} ByteAnalysis
+*/
+
+/**
+* @typedef {object} QualifiedByteCount
+* @property {string} QualifiedByteCount.name
+* @property {number[]} QualifiedByteCount.byteCounts 
+*/
+
+/**
+ * 
+ * @param {QualifiedByteCount[]} countData
+ * @returns {ByteAnalysis}
+ */
+function createByteAnalysis(countData) {
+    for (const dataEntry of countData) {
+        
+    }
+    //TODO
+    return {}
+}
+
 function main() {
-    /** @type string | undefined */
+    /** @type {string | undefined} */
     const filePath = process.argv[2]
 
     if (filePath == undefined) {
@@ -25,16 +86,47 @@ function main() {
     const fileContent = fs.readFileSync(filePath, {encoding: "utf-8", flag: "r"})
 
     
-    /** @type SearchFile */
+    /** @type {SearchFile} */
     const searchData = JSON.parse(fileContent)
 
+    /** @type {ByteCounts} */
+    const byteCounts = new Map()
+
+    for (const set of searchData.sets) {
+        byteCounts.set(set.name, loadSearchSet(set))
+    }
+
+    const fileSize = byteCounts.get(searchData.sets[0].name)?.length ?? 0
+
+    for (let i = 0; i < fileSize; i++) {
+        /** @type {QualifiedByteCount[]} */
+        const countData = []
+        
+        for (const set of searchData.sets) {
+            countData.push({
+                name: set.name,
+                byteCounts: byteCounts.get(set.name)?.[i] ?? []
+            })
+        }
     
+        const byteAnalysis = createByteAnalysis(countData)
+        //TODO: push into sorted Set
+    }
+
+   
     console.log(searchData)
+
+    
+
 
 
     fs.writeFileSync(outFilePath, JSON.stringify(searchData), {encoding: "utf-8", flag: "w"})
     console.log("Successfully analysed Data")
     console.log(`Log File Generated: ${outFilePath}`)
+}
+
+main()
+
 
     // Todo: Read files from Buckets
     // Compare Sets and document similarities between bits and bytes
@@ -68,6 +160,3 @@ function main() {
     //  textbox v overworld
     //  cutscene v overworld
     //  pause + textbox + cutscene v battle
-}
-
-main()
