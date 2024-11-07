42b8621da93fa1b404a1b48cdb0cfa315490a9c5
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2024-11-07 20:13:14 +0100

    Add Search Files

--- analysis/src/main.js
+++ analysis/src/main.js
@@ -399,37 +399,3 @@ function main() {
 }
 
 main()
-
-
-    // Todo: Read files from Buckets
-    // Compare Sets and document similarities between bits and bytes
-    // Eg. Load X Bytes from all Files, and compare for each bit / byte
-    // Comparison: Create Score
-    // Different Groupings (bytes only) eg. 0 as values 0x12, 0x13 and 1 has 0x00
-    // Each Result has Score values:
-    //      minSpread (min number of different values in set)
-    //      maxSpread (max number of different values in set) 
-    //      outlierPercentage (values that dont fit into calculation, eg. common value from 0 appears once in 1) (above 0.05 not counted as Result)
-
-    // Store everything in SortedMap
-    // When done, output map entries into file
-
-    // TODO: analysis functions
-    //  find bytes / bits (that are identifying across all buckets) eg. each bucket has a single unique value
-    //  ability to combine buckets for tests (eg. battle_mario, battle_luigi, battle_enemy vs levelup_mario, levelup_luigi)
-    //  maybe trough tags (battle, mario, luigi, overworld, movement, ...)
-    //  make sure that memory is taken, before buttons are pressed (eg. while you can still make inputs) (battle_mario_attack_before_press, battle_mario_attack_after_press)
-    //  ability to detect near misses
-
-    // maybe use many addresses for each detection and log unexpected deviations (they should all give the same result)
-    // prepare tests for each flag
-    //  mario v luigi overworld
-    //  battle v overworld
-    //  battle v powerup
-    //  mario_battle v luigi_battle
-    //  minigame v overworld
-    //  minigame v battle
-    //  pause v overworld
-    //  textbox v overworld
-    //  cutscene v overworld
-    //  pause + textbox + cutscene v battle
